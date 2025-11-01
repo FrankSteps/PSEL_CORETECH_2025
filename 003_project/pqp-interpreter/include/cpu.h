9f9f5afee041da77fb56fd55971cf64ec2acea93
GuilhermeAmancio <guilherme.amancio@dcomp.ufs.br>
2025-11-01 17:31:32 -0300

    atualização de definições em cpu.h
    Definição de constantes da arquitetura PQP e interface de CPU

--- 003_project/pqp-interpreter/include/cpu.h
+++ 003_project/pqp-interpreter/include/cpu.h
@@ -1,6 +1,14 @@
 #ifndef CPU_HEADER
 #define CPU_HEADER
 
+#include <stdint.h>
+
+//constantes úteis
+#define NUM_REGISTERS 16
+#define NUM_OPCODES 16
+#define MEMORY_SIZE 256
+#define EXIT_PC 0xF0F0 //endereço de término da simulação
+
 typedef struct Cpu Cpu;
 
 Cpu* cpu_create();
