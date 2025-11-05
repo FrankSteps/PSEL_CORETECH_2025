2570d421ba3a3588151f728070e00f562cc28326
Francisco Passos <franciscopassos.contato@gmail.com>
2025-11-05 19:59:14 -0300

    Refine CPU header file comments and constants
    Updated comments and definitions for clarity.

--- 003_project/pqp-interpreter/include/cpu.h
+++ 003_project/pqp-interpreter/include/cpu.h
@@ -1,18 +1,23 @@
+/*
+Definindo a estrutura básica da CPU
+*/
+
 #ifndef CPU_HEADER
 #define CPU_HEADER
 
 #include <stdint.h>
-
 #include "mem.h"
 
-//constantes úteis
-#define NUM_REGISTERS 16
-#define NUM_OPCODES 16
-#define MEMORY_SIZE 256
-#define EXIT_PC 0xF0F0 //endereço de término da simulação
+// constantes úteis
+#define NUM_REGISTERS 16   // Número total de registradores totais
+#define NUM_OPCODES 16     // quantidade de instruções possíveis
+#define MEMORY_SIZE 256    // tamanho da memória do pqp-interpreter
+#define EXIT_PC 0xF0F0     // endereço de término da simulação
 
+// declarando a estrutura da cpu -> declaração opaca
 typedef struct Cpu Cpu;
 
+// configuração base da CPU (criação, inicialização e execução)
 Cpu* cpu_create();
 void cpu_destroy(Cpu* cpu);
 void cpu_cycle(Cpu* cpu, Memory* mem);
