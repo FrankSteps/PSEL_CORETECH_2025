ebc98d8269356e94f4e367b8db1d92d18e929d52
Francisco Passos <franciscopassos.contato@gmail.com>
2025-11-05 20:08:14 -0300

    Refactor memory function declarations alignment
    Formatted function declarations for better readability.

--- 003_project/pqp-interpreter/include/mem.h
+++ 003_project/pqp-interpreter/include/mem.h
@@ -1,3 +1,13 @@
+/*
+ headers responsável por conter os artifícios de manipulação da memória da CPU
+
+ ele oferece as operações básicas para:
+   - criar e alocar a memória
+   - carregar o input na memória
+
+ O mesmo oferece o suporte para o gabage colector (artifício responsável por liberar a memória quando a mesma não está mais em uso)
+*/
+
 #ifndef MEM_HEADER
 #define MEM_HEADER
 
@@ -5,10 +15,10 @@
 
 typedef struct Memory Memory;
 
-Memory* mem_create(uint32_t size);
-void mem_destroy(Memory* mem);
-void mem_load_program(Memory* mem, char* file_path);
-uint8_t mem_read8(Memory* mem, uint16_t address);
-uint32_t mem_read32(Memory* mem, uint16_t address);
+Memory* mem_create(uint32_t size);                       
+void mem_destroy(Memory* mem);                           
+void mem_load_program(Memory* mem, char* file_path);    
+uint8_t mem_read8(Memory* mem, uint16_t address);        
+uint32_t mem_read32(Memory* mem, uint16_t address);     
 
 #endif
