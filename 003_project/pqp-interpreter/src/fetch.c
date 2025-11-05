ed227984508b0d736bd7b1ec98adb56bf903e2f5
Francisco Passos <franciscopassos.contato@gmail.com>
2025-11-05 20:33:13 -0300

    Document fetch function in fetch.c
    Added documentation comment for fetch function.

--- 003_project/pqp-interpreter/src/fetch.c
+++ 003_project/pqp-interpreter/src/fetch.c
@@ -1,9 +1,14 @@
+/*
+responsável pela primeira etapa do ciclo de execução da CPU.
+*/
+
 #include "cpu_internals.h"
 
 #include <stdio.h>
 
 #include "mem.h"
 
+// Função responsável pela busca da próxima instrução da memória para que a CPU para decodificá-la e executá-la.
 uint32_t fetch(Memory* mem, Cpu* cpu) {
     uint32_t instruction =  mem_read32(mem, cpu->pc);
 
