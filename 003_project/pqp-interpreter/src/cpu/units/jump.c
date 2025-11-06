9ec30cf90f4ba4bc7bc380a571b18a2c19bd876c
Francisco Passos <franciscopassos.contato@gmail.com>
2025-11-06 18:33:03 -0300

    Add jump instructions for CPU execution
    Implement jump instructions based on CPU flags G, L, and E.

--- 003_project/pqp-interpreter/src/cpu/units/jump.c
+++ 003_project/pqp-interpreter/src/cpu/units/jump.c
@@ -1,18 +1,27 @@
+/*
+ Implementa as instruções de salto (jump) da CPU, com base nas flags G, L e E.
+*/
+
+// incluindo instruções de CPU e memória
 #include "cpu_internals.h"
 #include "mem.h"
 
+// Faz um jump incondicional
 void exec_jmp(Cpu* cpu, Memory* mem, Instruction* instruction) {
     cpu->pc += (uint32_t)instruction->immediate;
 }
 
+// Faz um jump condicional se maior
 void exec_jg(Cpu* cpu, Memory* mem, Instruction* instruction) {
     if (cpu->flags & (1 << G_SHIFT)) exec_jmp(cpu, mem, instruction); 
 }
 
+// Faz um jump condicional se menor
 void exec_jl(Cpu* cpu, Memory* mem, Instruction* instruction) {
     if (cpu->flags & (1 << L_SHIFT)) exec_jmp(cpu, mem, instruction); 
 }
 
+// Faz um jump condicional se igual
 void exec_je(Cpu* cpu, Memory* mem, Instruction* instruction) {
     if (cpu->flags & (1 << E_SHIFT)) exec_jmp(cpu, mem, instruction); 
 }
